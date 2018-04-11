71f6331e17b1c31ff8897876b6697c2b4988e8f8
android <android@xiaoyifei.lan>
2018-04-11 20:56:04 +0800

    remove old things

--- controller/version.sh
+++
@@ -1,6 +0,0 @@
-apt-get update
-apt-get install -y --force-yes python-software-properties 
-echo "deb http://ubuntu-cloud.archive.canonical.com/ubuntu trusty-updates/juno main">> /etc/apt/sources.list.d/ubuntu-cloud-archive-juno-trusty.list
-apt-get install -y --force-yes ubuntu-cloud-keyring
-apt-get update && apt-get dist-upgrade
-reboot
